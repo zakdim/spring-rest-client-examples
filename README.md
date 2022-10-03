045c4c76cc8a822d14d8300baaafedf99237a1fe
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-10-02 21:34:19 -0400

    s23-365 Init project for intro to RESTFul Web Services

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spring Rest Client Examples
+
+An example application for `Section 23: Introduction to RESTFul Web Services` of the Udemy course:
+[Spring Framework 5 - Beginner to Guru](https://www.udemy.com/course/spring-framework-5-beginner-to-guru/)
+
---
+++ src/test/java/guru/springframework/springrestclientexamples/SpringRestClientExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springrestclientexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestClientExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
